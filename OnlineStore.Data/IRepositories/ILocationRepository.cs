1b716d231b00f5ecee9d03e8c8e2a03e71d5179b
temurofficial07 <temuraslonboyev@gmail.com>
2022-09-10 17:20:04 +0500

    Add Data

---
+++ OnlineStore.Data/IRepositories/ILocationRepository.cs
@@ -0,0 +1,14 @@
+﻿using OnlineStore.Data.IRepositories.IBaseRepositories;
+using OnlineStore.Domain.Entities.Locations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Data.IRepositories
+{
+    public interface ILocationRepository : IGenericRepository<Location>
+    {
+    }
+}
---
+++ OnlineStore.Data/IRepositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using OnlineStore.Data.IRepositories.IBaseRepositories;
+using OnlineStore.Domain.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Data.IRepositories
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+    }
+}
